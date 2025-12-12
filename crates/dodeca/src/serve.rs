d92d72d6ae8dfd2e4ef43519f10fb4076da39fe6
Amos Wenger <amos@bearcove.eu>
2025-12-12 13:13:30 +0100

    Comment out poorly-thought-out dioxus integration

--- crates/dodeca/src/serve.rs
+++ crates/dodeca/src/serve.rs
@@ -1319,48 +1319,48 @@ pub fn devtools_urls() -> (String, String) {
 
 /// Embedded JS snippets required by Dioxus WASM
 const SNIPPETS: &[(&str, &str)] = &[
-    (
-        "snippets/dioxus-cli-config-e5fab7f8a0eb9fbb/inline0.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-cli-config-e5fab7f8a0eb9fbb/inline0.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-interpreter-js-267e64abc8a52eaa/inline0.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/inline0.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/patch_console.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/patch_console.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/hydrate.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/hydrate.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/set_attribute.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/set_attribute.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-web-807c31b5ece9dd6a/inline0.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-web-807c31b5ece9dd6a/inline0.js"
-        ),
-    ),
-    (
-        "snippets/dioxus-web-807c31b5ece9dd6a/src/js/eval.js",
-        include_str!(
-            "../../../crates/dodeca-devtools/pkg/snippets/dioxus-web-807c31b5ece9dd6a/src/js/eval.js"
-        ),
-    ),
+    // (
+    //     "snippets/dioxus-cli-config-e5fab7f8a0eb9fbb/inline0.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-cli-config-e5fab7f8a0eb9fbb/inline0.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-interpreter-js-267e64abc8a52eaa/inline0.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/inline0.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/patch_console.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/patch_console.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/hydrate.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/hydrate.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/set_attribute.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-interpreter-js-267e64abc8a52eaa/src/js/set_attribute.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-web-807c31b5ece9dd6a/inline0.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-web-807c31b5ece9dd6a/inline0.js"
+    //     ),
+    // ),
+    // (
+    //     "snippets/dioxus-web-807c31b5ece9dd6a/src/js/eval.js",
+    //     include_str!(
+    //         "../../../crates/dodeca-devtools/pkg/snippets/dioxus-web-807c31b5ece9dd6a/src/js/eval.js"
+    //     ),
+    // ),
 ];
 
 /// Get devtools asset content by path (for RPC serving)
